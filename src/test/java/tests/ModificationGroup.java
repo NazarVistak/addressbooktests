cfa16e9fcb6236457c667ececb0f82b61a137abf
Nazar Vistak <vistak.nazar@gmail.com>
2019-11-10 21:10:58 +0200

    Додано перевірки на правильну кількість груп до і після (додавання,модифікації та видалення)

--- src/test/java/tests/ModificationGroup.java
+++ src/test/java/tests/ModificationGroup.java
@@ -1,19 +1,25 @@
 package tests;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ModificationGroup extends TestBase {
 @Test
   public void testGroupModification(){
   app.getNavigationHelper().groupsOpening();
-  if(! app.getGroupHelper().isThereAGroup()){
-    app.getGroupHelper().createGroup(null);
+  int before = app.getGroupHelper().getGroupCount();
+    if(! app.getGroupHelper().isThereAGroup()){
+  app.getGroupHelper().createGroup(null);
   }
   app.getGroupHelper().selectGroup();
   app.getGroupHelper().initGroupModification();
   app.getGroupHelper().fillingGroupName("test");
   app.getGroupHelper().submitGroupModification();
   app.getGroupHelper().returnToGroupPage();
+  int after = app.getGroupHelper().getGroupCount();
+  Assert.assertEquals(after ,  before);
+  System.out.println("Number of groups BEFORE = " + before);
+  System.out.println("Number of groups AFTER = " + after);
 }
 }
