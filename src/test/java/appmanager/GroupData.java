22124fcb6985fa6843a6705e4a32cc5991d0ac36
Nazar Vistak <vistak.nazar@gmail.com>
2019-11-11 19:54:19 +0200

    Перетворили колекцію в множества, для перевірки співпадінь по id, щоб вони не змінювались при модифікації

--- src/test/java/appmanager/GroupData.java
+++ src/test/java/appmanager/GroupData.java
@@ -1,15 +1,26 @@
 package appmanager;
 
+import tests.ModificationGroup;
+
 import java.util.Objects;
 
-public class GroupData {
+public class GroupData  {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
-  public GroupData(String name, String header, String footer) {
+
+  public GroupData(String id,String name, String header, String footer) {
+    this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+    public GroupData(String name, String header, String footer) {
+      this.id = null;
+      this.name = name;
+      this.header = header;
+      this.footer = footer;
   }
   public String getName(){
     return name;
@@ -21,11 +32,8 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
+  public String getId() {
+    return id;
   }
 
   @Override
@@ -33,11 +41,21 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+    return Objects.equals(id, groupData.id) &&
+            Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(name);
+    return Objects.hash(id, name);
   }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
 }
--- src/test/java/tests/ModificationGroup.java
+++ src/test/java/tests/ModificationGroup.java
@@ -3,6 +3,7 @@ import appmanager.GroupData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ModificationGroup extends TestBase {
@@ -10,20 +11,25 @@ public class ModificationGroup extends TestBase {
   public void testGroupModification(){
   app.getNavigationHelper().groupsOpening();
 
-
-    if(! app.getGroupHelper().isThereAGroup()){
+  if(! app.getGroupHelper().isThereAGroup()){
   app.getGroupHelper().createGroup(null);
 
     }
   List<GroupData> before = app.getGroupHelper().getGroupList();
   app.getGroupHelper().selectGroup(before.size() - 1);
   app.getGroupHelper().initGroupModification();
+  GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test","test1","test2");
   app.getGroupHelper().fillingGroupName("test");
   app.getGroupHelper().submitGroupModification();
   app.getGroupHelper().returnToGroupPage();
+
   List<GroupData> after = app.getGroupHelper().getGroupList();
   Assert.assertEquals(after.size() ,  before.size());
   System.out.println("Number of groups BEFORE = " + before);
   System.out.println("Number of groups AFTER = " + after);
+
+  before.remove(before.size() - 1);
+  before.add(group);
+  Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 }
 }
